e2fde72ea16cafa7db3e5069f9dd79f2fc0803e3
alumne <alexbonavila@iesebre.com>
2016-06-14 18:26:59 +0200

    primer comit + dependencies

---
+++ app/src/main/java/com/alexbonavila/alumne/recuperacioonesignal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alexbonavila.alumne.recuperacioonesignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
